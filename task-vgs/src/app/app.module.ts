8953b7fab2ecb87fa97103164b88169bf0e832cd
alberto-vgs <alberto_b03@outlook.com>
2024-02-11 22:14:25 +0000

    others components

--- task-vgs/src/app/app.module.ts
+++ task-vgs/src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
 import { TaskComponent } from './components/task/task.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
+import { NavBarComponent } from './components/nav/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { NotFoundPageComponent } from './components/pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
     TaskFormComponent,
-    TaskComponent
+    TaskComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    NavBarComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
